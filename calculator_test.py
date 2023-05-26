e399cecb3ca3e1f63552a01b166be8bd95d51399
angelomichaelx <michaelangelobiag23@gmail.com>
2023-05-26 20:37:05 +0800

    adding test py file, final

--- calculator_test.py
+++ calculator_test.py
@@ -1,3 +1,5 @@
+#improting calculator from calculator_main.py file
 from calculator_main import calculator
+#run calculator to run the calculator with OOP concept from main file 
 calculator = calculator()
 calculator.run_calculator()
