ea0b84e9816300897d7b1819633d18a2b3e248d8
angelomichaelx <michaelangelobiag23@gmail.com>
2023-05-26 19:12:56 +0800

    methods for (addition, subtruction, multiplication, division)

--- calculator.py
+++ calculator.py
@@ -8,5 +8,21 @@ class calculator:
 # constructor
     def __init__(self):
         self.operators = ('+','-','*','/')
-        
+#addition
+    def addition(self, num1, num2):
+        return num1 + num2
+#subtraction
+    def subtraction(self, num1, num2):
+        return num1 - num2
+#multiplication 
+    def multiplication(self, num1, num2):
+        return num1 * num2
+#division
+    def division(self, num1, num2):
+        if num2 == 0:
+            raise ZeroDivisionError
+        else:
+            return num1 / num2
+
+
 
